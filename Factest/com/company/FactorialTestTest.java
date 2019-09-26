c86d22b80c5f1e05d8e632d7a3498b2162c85b22
sune <sune.bro@ka-net.dk>
2019-09-26 10:40:19 +0200

    added tests

--- Factest/com/company/FactorialTestTest.java
+++ Factest/com/company/FactorialTestTest.java
@@ -1,12 +1,41 @@
 package com.company;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
-public class FactorialTestTest extends TestCase {
-    public void  testbergn() {
-        int resultat = FactorialTest.
-    }
 
+public class FactorialTestTest extends TestCase {
+    public void TestFactorial () {
+        FactorialTest resultat;
+
+        //tester for 0
+        //input 0
+        //forventet output 0
+
+        resultat = new FactorialTest(0);
+        Assert.assertEquals(0, resultat);
+
+        //tester for kommatal
+        //input 3.5
+        //forventet output 17.5
+        resultat = new FactorialTest(3.5);
+        Assert.assertEquals(17.5, resultat);
+
+        //tester for negative tal
+        //input -3
+        //forventet output
+
+        resultat = new FactorialTest(-3);
+        Assert.assertEquals(-15, resultat);
+
+
+
+
+
+
+
+
+    }
 
 
 
--- src/com/company/FactorialTest.java
+++ src/com/company/FactorialTest.java
@@ -2,12 +2,15 @@ package com.company;
 
 public class FactorialTest {
 
+    public FactorialTest(double i) {
+
+    }
+
     public static void main(String[] args) {
 	// write your code here
-        for (int i = 1; i <=200 ; i = i * 5) {
-            System.out.println(i);
+
 
         }
 
     }
-}
+
